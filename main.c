6ba1bacb437b1a4b0af1d143efb9a9dec0397299
dirkarnez <smalldirkalex@gmail.com>
2024-11-09 23:06:50 +0800

    - wip

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#ifdef __AVR_ATmega328P__
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
@@ -24,37 +25,6 @@ UBRRL = (uint8_t)( (F_CPU + BAUD_RATE * 4L) / (BAUD_RATE * 8L) - 1 );
 https://github.com/search?q=repo%3Aarduino%2FArduinoCore-avr+UBRRL&type=code
 */
 
-
-
-#define __must_be(e, msg)	(				\
-	0 * (int)sizeof(						    \
-		struct {						          \
-			_Static_assert((e), msg);			\
-			char ISO_C_forbids_a_struct_with_no_members__;	\
-		}							\
-	)								\
-)
-
-#define _a (1 * (F_CPU / 1024))
-#define AA (_a) + __must_be((_a) > 0, "")
-
-int main(void) { 
-
-}
-
-
-// _Static_assert does not work with in
-#define UBRR_VALUE(USART_BAUDRATE) ({  \
-	_Static_assert(USART_BAUDRATE <= 115200, "Cannot larger than 115200"); \
-	__UBRR_VALUE(USART_BAUDRATE); \
-	\
-})
-
-#ifdef __AVR_ATmega328P__
-#include <avr/io.h>
-
-void Timer_1_Delay();		// Prototype for Delay Function 
-
 int main(void)
 {
 	DDRB = 1<<0;			// PB0 as Output
@@ -65,19 +35,6 @@ int main(void)
 		// PORTB ^= (1<<0);	// Toggle
 		PORTB = 0xFF;
     }
-}
-
-void Timer_1_Delay()
-{
-// Timer 1 16 bit timer with prescaler = 1024
-// 1 * 16M / 1024 = 15625
-	OCR1AH = 0x3D;		// CTC OCR
-	OCR1AL = 0x08;		// 15625 - 1 = 0x3D08
-	TCCR1A = 0x00;		// CTC Mode
-	TCCR1B = 0x0D;		// CTC Mode, Prescaler = 1024
-	while ((TIFR1 &(1<<OCF1A))==0);	// Continue Until Overflow
-	TCCR1B = 0x00;		// Stop Timer 1
-	TIFR1 = (1<<OCF1A);	// Reset OCR1A
 }
 
 #else
@@ -101,4 +58,3 @@ int main(int argc, char** argv) {
 }
 
 #endif
-
